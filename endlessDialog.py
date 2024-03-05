a6856cc1239e5aeaa241ccb3fb063b6e98f2d42a
Khalid <alhusseinikhalid7@gmail.com>
2024-03-04 23:26:36 -0500

    d

--- endlessDialog.py
+++ endlessDialog.py
@@ -1,1 +1,21 @@
 #Endless Dialog (Literally)
+
+
+import tkinter as tk
+from tkinter import messagebox
+
+
+
+
+def InfiniteLoops():
+    while True:
+        root = tk.Tk()
+        root.withdraw()
+        messagebox.showerror("ERROR")
+
+
+
+
+
+
+InfiniteLoops()
